d229d9dcbaddde1e0e6b399078563da31e9b1f50
sanieni6 <luissanchezzamora23@gmail.com>
2023-10-03 22:27:03 -0500

    update auth

--- Routes/user.js
+++ Routes/user.js
@@ -1,11 +1,12 @@
 import Express from "express";
 import { updateUser, deleteUser, getAllUser, getSingleUser } from "../Controllers/userController.js";
+import { authenticate, restrict } from "../auth/verifyToken.js";
 
 const router = Express.Router();
 
-router.get("/", getAllUser);
-router.get("/:id", getSingleUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/", authenticate, restrict(["admin"]), getAllUser);
+router.get("/:id", authenticate, restrict(["patient"]), getSingleUser);
+router.put("/:id", authenticate, restrict(["patient"]), updateUser);
+router.delete("/:id", authenticate, restrict(["patient"]), deleteUser);
 
 export default router;
