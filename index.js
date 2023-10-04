95b91ee33e1499f200c3316b567d28b999230db0
sanieni6 <luissanchezzamora23@gmail.com>
2023-10-03 22:29:12 -0500

    Add new funct

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRoute from './Routes/auth.js';
 import userRoute from './Routes/user.js';
+import doctorRoute from './Routes/doctor.js';
+import reviewRoute from './Routes/review.js';
+
 
 dotenv.config()
 
@@ -41,6 +44,9 @@ app.use(cookieParser())
 app.use(cors(corsOptions))
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/users', userRoute)
+app.use('/api/v1/doctors', doctorRoute)
+app.use('/api/v1/reviews', reviewRoute)
+
 
 
 app.listen(port, () => {
